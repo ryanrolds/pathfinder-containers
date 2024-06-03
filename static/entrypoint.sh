59a48e3e3490b9ae9edde23af21bf97c9a307957
Ryan R. Olds <ryanrolds@gmail.com>
2024-06-03 12:00:33 -0700

    Adjusting entrypoint file

--- static/entrypoint.sh
+++ static/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 crontab /var/crontab.txt
 envsubst '$DOMAIN' </etc/nginx/templateSite.conf >/etc/nginx/sites_enabled/site.conf
 envsubst '$PATHFINDER_SOCKET_HOST' </etc/nginx/templateNginx.conf >/etc/nginx/nginx.conf
-envsubst  </etc/php7/php-fpm.d/zzz_custom.conf.temp >/etc/php7/php-fpm.d/zzz_custom.conf
-envsubst  </etc/zzz_custom.ini >/etc/php7/conf.d/zzz_custom.ini
-htpasswd   -c -b -B  /etc/nginx/.setup_pass pf "$APP_PASSWORD"
+envsubst </etc/php7/php-fpm.d/zzz_custom.conf.temp >/etc/php7/php-fpm.d/zzz_custom.conf
+envsubst </etc/zzz_custom.ini >/etc/php7/conf.d/zzz_custom.ini
+htpasswd -c -b -B  /etc/nginx/.setup_pass pf "$APP_PASSWORD"
 exec "$@"
